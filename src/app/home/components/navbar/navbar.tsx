e9d09f1c2f3aefcef4114f763f5a950038a4707b
greglobrigo <golivefun@gmail.com>
2023-10-09 22:07:51 +0800

    Clean up on home page.

--- src/app/home/components/navbar/navbar.tsx
+++ src/app/home/components/navbar/navbar.tsx
@@ -1,10 +1,9 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
 export default function Navbar () {
-  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="gpt3__navbar">
@@ -12,29 +11,8 @@ export default function Navbar () {
         <div className="gpt3__navbar-links_logo">
           <h1>glxd</h1>
         </div>
-        <div className="gpt3__navbar-links_container">
-          <p><a href="#home"></a></p>
-          <p><a href="#about"></a></p>
-          <p><a href="#possibility"></a></p>
-          <p><a href="#features"></a></p>
-          <p><a href="#blog"></a></p>
-        </div>
       </div>
       <div className="gpt3__navbar-menu">
-        {toggleMenu
-          ? <RiCloseLine className="invisible" color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line className="invisible" color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
-        {toggleMenu && (
-        <div className="gpt3__navbar-menu_container scale-up-center">
-          <div className="gpt3__navbar-menu_container-links">
-            <p><a href="#home"></a></p>
-            <p><a href="#about"></a></p>
-            <p><a href="#possibility"></a></p>
-            <p><a href="#features"></a></p>
-            <p><a href="#blog"></a></p>
-          </div>
-        </div>
-        )}
       </div>
     </div>
   );
