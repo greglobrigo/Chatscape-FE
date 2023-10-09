5b1a52c289753895f31c4780afde5d8860d8b048
greglobrigo <golivefun@gmail.com>
2023-10-09 19:10:22 +0800

    Added home elements and log in component.

---
+++ src/app/Home/components/login-form/login-form.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { useRef } from 'react';
+import axios from 'axios';
+import { AxiosResponse, AxiosError } from 'axios';
+
+
+function LoginForm() {
+
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+     const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        console.log('clicked')
+        const email = emailRef.current?.value;
+        const password = passwordRef.current?.value;
+        await axios({
+            method: 'post',
+            url: 'http://localhost:3001/users/login',
+            data: {
+                email: email,
+                password: password
+            }
+        }).then((res:AxiosResponse) => {
+            console.log(res.data);
+        }).catch((err:AxiosError) => {
+            console.log(err.response?.data);
+        })
+    }
+
+    return (
+        <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Username
+            </label>
+            <input ref={emailRef} name="email"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input ref = {passwordRef} name="password"
+            className="shadow appearance-none borde rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+          </div>
+          <div className="flex items-center justify-between">
+            <button onClick={handleSubmit}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Sign In
+            </button>
+            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    );
+}
+
+export default LoginForm;
--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,9 +1,9 @@
-"use client"
 import Navbar from "./components/navbar/navbar";
 import './home.css';
 import { Fade } from 'react-awesome-reveal'
 import home from './home.svg'
 import Image from 'next/image'
+import LoginForm from "./components/login-form/login-form";
 
 
 export default function Home() {
@@ -13,13 +13,18 @@ export default function Home() {
             <Navbar />
             <div className="home__header section__padding" id="home">
                 <div className="home__header-content">
-                    <div className="bg color-[black]">
+                    <div className="bg color-[black] text-center">
                         <h1>Welcome to Chatscape!</h1>
+                        <div className="justify-center">
                         <Image src="/home.svg" alt="home" width={500} height={500} />
+                        </div>
                     </div>
                 </div>
 
-                <div className="home__header-image flex-col">
+                <div className="home__header-content">
+                    <h1>Login</h1>
+                    {/* write me a login and logout input */}
+                    <LoginForm />
                 </div>
             </div>
         </>
