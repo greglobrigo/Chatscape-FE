6a2e907fb8a02d0e0e1efba979f640df78ec67fb
greglobrigo <golivefun@gmail.com>
2023-11-02 20:11:30 +0800

    Modified header searchbar to prepare for searching for groups.

--- src/app/chat/components/header.tsx
+++ src/app/chat/components/header.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
 
 export default function Header({ currentUser }: any) {
+
+    const handleSearchPublicChats = (e: any) => {
+        console.log(e);
+    }
+
+
     return (
         <div id="header" className='px-5 py-5 flex justify-between items-center bg-white border-b-2 shadow-lg rounded-lg'>
             <div className="font-semibold text-2xl">Chatscape</div>
             <div className="w-1/2">
-                <input
+                <input onChange={(e) => handleSearchPublicChats(e.target.value)}
                     type="text"
                     name=""
                     id=""
-                    placeholder="search IRL"
+                    placeholder="search public chats"
                     className="rounded-2xl bg-gray-100 py-3 px-5 w-full"
                 />
             </div>
