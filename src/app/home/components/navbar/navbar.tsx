910818928a1679a102705de8b0970b04de096923
greglobrigo <golivefun@gmail.com>
2023-11-11 16:30:51 +0800

    Added backlikn to portfolio.

--- src/app/home/components/navbar/navbar.tsx
+++ src/app/home/components/navbar/navbar.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
-export default function Navbar () {
+export default function Navbar() {
 
   return (
     <div className="gpt3__navbar absolute top-0">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <h1>glxd</h1>
+          <a href="https://portfolio-greglobrigo.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <h1 className='cursor-pointer'>glxd</h1>
+          </a>
         </div>
       </div>
       <div className="gpt3__navbar-menu">
